89cb95d5f17f7aa3c0d503e2124362a18a3402e1
mbalyaba <mbalyaba@gmail.com>
2013-05-23 03:25:40 -0700

    delete debug mode

--- modules/Android/BarcodeScanner/target/generated-sources/r/com/google/zxing/client/android/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.zxing.client.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
