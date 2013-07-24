3afce83dd127057211590e81213de9fbacb0b88a
dgruzda <dgruzda@gmail.com>
2013-07-24 10:32:12 +0300

    Fixed clear credentials feature on Android 4

--- modules/Android/Tester/src/main/java/io/appery/tester/PreferencesActivity.java
+++ modules/Android/Tester/src/main/java/io/appery/tester/PreferencesActivity.java
@@ -9,11 +9,12 @@ import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-public class PreferencesActivity extends PreferenceActivity implements OnPreferenceChangeListener {
+public class PreferencesActivity extends PreferenceActivity implements OnPreferenceChangeListener, OnPreferenceClickListener {
 
     public void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -35,23 +36,36 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
             preference.setSummary(p.getText());
         } else if (preference instanceof ButtonPreference) {
             preference.setOnPreferenceChangeListener(this);
+            preference.setOnPreferenceClickListener(this);
+        }
+    }
+
+    private void clearCredentialPreferences() {
+    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs != null) {
+            try {
+                Editor editor = prefs.edit();
+                editor.putString(Constants.PREFERENCES.USERNAME, null);
+                editor.putString(Constants.PREFERENCES.PASSWORD, null);
+                editor.commit();
+            } catch (Exception e) {
+                Log.e(PreferencesActivity.class.getName(), "Set preference error");
+            }
         }
     }
 
     @Override
+	public boolean onPreferenceClick(Preference preference) {
+    	if (Constants.PREFERENCES.USERNAME.equals(preference.getKey())) {
+            clearCredentialPreferences();
+        }
+    	return true;
+	}
+
+	@Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (Constants.PREFERENCES.USERNAME.equals(preference.getKey())) {
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-            if (prefs != null) {
-                try {
-                    Editor editor = prefs.edit();
-                    editor.putString(Constants.PREFERENCES.USERNAME, null);
-                    editor.putString(Constants.PREFERENCES.PASSWORD, null);
-                    editor.commit();
-                } catch (Exception e) {
-                    Log.e(PreferencesActivity.class.getName(), "Set preference error");
-                }
-            }
+            clearCredentialPreferences();
             return true;
         }
         preference.setSummary(String.valueOf(newValue));
